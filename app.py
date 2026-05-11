e1f8246cf799d8d9be6133be0f18c31aa6ec67a6
Saurabh Mallik <saurabhmallik07@gmail.com>
2026-05-11 14:02:01 +0530

    Updated app for Render deployment

--- app.py
+++ app.py
@@ -442,4 +442,5 @@ def api_status():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False, host="0.0.0.0", port=5000, threaded=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, use_reloader=False, host="0.0.0.0", port=port, threaded=True)
